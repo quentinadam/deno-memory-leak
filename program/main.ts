229021540bf6ed6c18ee75df4a3dbe49a4076efb
Quentin Adam <quentinadam@gmail.com>
2023-03-22 18:41:27 -0400

    Minor reformatting

--- program/main.ts
+++ program/main.ts
@@ -1,16 +1,13 @@
 console.log('Deno version', Deno.version.deno);
 
-let timestamp = new Date();
-
 const file = new URL('./hello.txt', import.meta.url).pathname;
+
+let timestamp = new Date();
 
 while (true) {
   if (Date.now() >= timestamp.valueOf()) {
     const bytes = Deno.memoryUsage().rss;
-    console.log(
-      timestamp.toISOString(),
-      Math.floor(bytes / (1024 * 1024) * 10) / 10,
-    );
+    console.log(timestamp.toISOString(), Math.floor(bytes / (1024 * 1024) * 10) / 10);
     timestamp = new Date(timestamp.valueOf() + 1000);
   }
   await Deno.readFileSync(file);
